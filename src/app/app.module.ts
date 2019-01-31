f63317c94f45afe0b93f66532adea14c798a29a1
Michael Brown and Quinn Miller <michael@>
2019-01-31 11:02:55 -0800

    add routing to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
+
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AdminComponent } from './admin/admin.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
