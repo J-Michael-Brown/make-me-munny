da1e2c86464df82bd0bf7ca29718f1c64976ee10
Michael Brown and Quinn Miller <michael@>
2019-01-31 11:01:48 -0800

    add routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AdminComponent } from './admin/admin.component';
+import { AboutComponent } from './about/about.component';
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
