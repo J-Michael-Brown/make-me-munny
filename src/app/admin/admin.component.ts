b4710019bff27a98b6f0f5b6bf01ed5dcbbf189c
Michael Brown and Quinn Miller <michael@>
2019-01-31 14:48:34 -0800

    connect update forms to firebase

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'app/models/project.model';
+import { ProjectsService } from '../projects.service'
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [ProjectsService]
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  projects: FirebaseListObservable<any[]>;
+  constructor(private projectsService: ProjectsService) { }
 
   ngOnInit() {
+    this.projects = this.projectsService.getProjects();
   }
 
+  submitForm(title: string, username: string, description: string) {
+    var newProject: Project = new Project(title, username, description);
+    this.projectsService.addProject(newProject);
+  }
 }
