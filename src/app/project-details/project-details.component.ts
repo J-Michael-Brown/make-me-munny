93f930318e802926ecba84d51f04b8b9a150e12c
Michael Brown and Quinn Miller <quinn@>
2019-01-31 15:04:13 -0800

    connect project-detail component

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,15 +1,31 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { ProjectsService } from '../projects.service';
+import { Project } from 'app/models/project.model';
+
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.css']
+  styleUrls: ['./project-details.component.css'],
+  providers: [ProjectsService]
 })
 export class ProjectDetailsComponent implements OnInit {
 
-  constructor() { }
+  projectToDisplay;
+
+  constructor(private route: ActivatedRoute, private location: Location, private projectsService: ProjectsService) {}
 
   ngOnInit() {
+    let projectId: string;
+    this.route.params.forEach((urlParameters)=> {
+
+    projectId = urlParameters['id'];
+    });
+    this.projectToDisplay = this.projectsService.getProjectById(projectId);
+    console.log(this.projectToDisplay)
   }
 
 }
