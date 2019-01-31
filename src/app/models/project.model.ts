dbc37ef85c9a0f3971d9eca90505d642d2068e53
Michael Brown and Quinn Miller <michael@>
2019-01-31 14:49:37 -0800

    update project model

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -8,7 +8,7 @@ export class Project {
   public rewards: Reward[];
   public pledges: Pledge[];
 
-  constructor(public title: string, public username: string, public description: string, public associates: string[], public catagories: string[] = []){}
+  constructor(public title: string, public username: string, public description: string, public associates: string[] = [], public catagories: string[] = []){}
 
 
   startProject(milestones: Milestone[], rewards: Reward[]){
