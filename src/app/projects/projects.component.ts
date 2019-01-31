ccfe5a2d7c99929b4405c63f7d6bf460572b0c09
Michael Brown and Quinn Miller <michael@>
2019-01-31 14:27:40 -0800

    show all projects on page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../projects.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Project } from '../models/project.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectsService]
 })
+
+
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  projects: FirebaseListObservable<any[]>;
+
+  constructor(private router: Router, private projectsService: ProjectsService) {}
+
+  goToProjectDetails(clickedProject: Project) {
+    // this.router.navigate(['projects', clickedProject.$key]);
+  }
 
   ngOnInit() {
+    this.projects = this.projectsService.getProjects();
   }
 
 }
